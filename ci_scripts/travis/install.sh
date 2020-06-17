71ec0b7fd9c583a7dd158370dba4e3f22b4f79ae
Pawel Ksieniewicz <pawel@ksienie.com>
2020-06-17 23:24:22 +0200

    Another try.

--- ci_scripts/travis/install.sh
+++ ci_scripts/travis/install.sh
@@ -1,3 +1,3 @@
-pip install pytest pytest-cov codecov matplotlib coverage==4.4 coveralls tqdm
+pip install pytest pytest-cov codecov matplotlib coverage==4.4 coveralls tqdm scikit-learn
 pip install --upgrade pytest
 python setup.py develop
