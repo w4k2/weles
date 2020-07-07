1540fdd927ad2ef902c68f432d47b37d55d4f75f
tibetansandfox <pawel.zyblewski@pwr.edu.pl>
2020-07-07 21:59:44 +0200

    Data v0.0.1

--- weles/utils/Data.py
+++ weles/utils/Data.py
@@ -4,6 +4,80 @@ Class for preparing datasets for the evaluator.
 
 # imports
 import numpy as np
+import os
+import re
 
 class Data():
-    pass
+    def __init__(self, selection, path="../../datasets/"):
+        self.selection = selection
+        self.path = path
+
+    def load(self):
+        """
+        description
+        """
+        # load files
+        files = self.__dir2files(path=self.path)
+        datasets = {}
+
+        if type(self.selection) == tuple:
+            tag_filter = self.selection[1]
+            for file in files:
+                ds, dbname, tags = self.__csv2Xy(file)
+                if self.selection[0] == 'all':
+                    is_good = all(elem in tags for elem in tag_filter)
+                if self.selection[0] == 'any':
+                    is_good = any(elem in tags for elem in tag_filter)
+                if is_good:
+                    datasets[dbname] = ds
+
+        # Load datasets by name
+        elif type(self.selection) == list:
+            for file in files:
+                ds, dbname, _ = self.__csv2Xy(file)
+                is_good = dbname in self.selection
+                if is_good:
+                    datasets[dbname] = ds
+        else:
+            print("Provide a list or a tuple")
+        return datasets
+
+    def __tags4Xy(self, X, y):
+        tags = []
+        numberOfFeatures = X.shape[1]
+        numberOfSamples = len(y)
+        numberOfClasses = len(np.unique(y))
+        if numberOfClasses == 2:
+            tags.append("binary")
+        else:
+            tags.append("multi-class")
+        if numberOfFeatures >= 8:
+            tags.append("multi-feature")
+
+        # Calculate ratio
+        ratio = [0.0] * numberOfClasses
+        for y_ in y:
+            ratio[y_] += 1
+        ratio = [int(round(i / min(ratio))) for i in ratio]
+        if max(ratio) > 3:
+            tags.append("imbalanced")
+        else:
+            tags.append("balanced")
+        return tags
+
+
+    def __csv2Xy(self, path):
+        ds = np.genfromtxt(path, delimiter=",")
+        X = ds[:, :-1]
+        y = ds[:, -1].astype(int)
+        dbname = path.split("/")[-1].split(".")[0]
+        tags = self.__tags4Xy(X, y)
+        return ds, dbname, tags
+        # return datasets
+
+    def __dir2files(self, path, extention="csv"):
+        return [
+            path + x
+            for x in os.listdir(path)
+            if re.match("^([a-zA-Z0-9-_])+\.%s$" % extention, x)
+        ]
--- weles/utils/__init__.py
+++ weles/utils/__init__.py
@@ -1,4 +1,3 @@
 from .Data import Data
-from .diversity import entropy_measure_e
 
-__all__ = ["Data", "entropy_measure_e"]
+__all__ = ["Data"]
