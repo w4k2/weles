562c961136952754483aff256414080d0ae1f2ee
Dominika Sułot <domi@pop-os.localdomain>
2020-07-07 17:35:59 +0200

    evaluator structure added

--- weles/evaluation/Evaluator.py
+++ weles/evaluation/Evaluator.py
@@ -1,2 +1,32 @@
+"""
+Class description
+
+Date:
+Authors:
+"""
+
+# imports
+
+
 class Evaluator():
-  pass
+    def __init__(self, datasets, protocol):
+        self.datasets = datasets
+        self.protocol = protocol
+
+    def process(self, clfs):
+        """
+        description
+
+        clfs: dictonary that contains estimators names and objects
+              ["name"] : obj
+        """
+        pass
+
+    def score(self, metrcis):
+        """
+        description
+
+        metrics: dictonary that contains metrics names and functions
+                 ["name"] : function
+        """
+        pass
