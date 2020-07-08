d3e727b833991ac1e822031bae1d804b0cbf50a1
Dominika Sułot <domi@pop-os.localdomain>
2020-07-08 16:01:47 +0200

    score function implemented

--- weles/evaluation/Evaluator.py
+++ weles/evaluation/Evaluator.py
@@ -44,7 +44,7 @@ class Evaluator():
                     clf = clone(self.clfs[clf_name])
                     clf.fit(X[train], y[train])
                     y_pred = clf.predict(X[test])
-                    self.predictions[dataset_id, clf_id, fold_id] = [y_pred]
+                    self.predictions[dataset_id, clf_id, fold_id] = y_pred
 
         return self
 
@@ -56,11 +56,27 @@ class Evaluator():
         metrics: dictonary that contains metrics names and functions
                  ["name"] : function
         """
+        self.metrics = metrics
         _ = (len(self.datasets), len(self.clfs), len(metrics))
-
-        self.scores = np.zeros(_)
-
         # Flatten or not...
         if return_std:
             self.stds = np.zeros(_)
+
+        self.scores = np.zeros(_)
+        m, k, random_state = self.protocol
+        for dataset_id, dataset_name in enumerate(self.datasets):
+            for clf_id, clf_name in enumerate(self.clfs):
+                for metric_id, metric_name in enumerate(self.metrics):
+                    partial_scores = np.zeros([m*k])
+                    for i in range(m*k):
+                        y_test = self.true_values[dataset_id, i]
+                        y_pred = self.predictions[dataset_id, clf_id, i]
+                        partial_scores[i] = self.metrics[metric_name](y_test, y_pred)
+                    self.scores[dataset_id, clf_id, metric_id] = np.mean(partial_scores)
+                    if return_std:
+                        self.stds[dataset_id, clf_id, metric_id] = np.std(partial_scores)
+
+        if return_std:
             return self.scores, self.stds
+        else:
+            return self.scores
